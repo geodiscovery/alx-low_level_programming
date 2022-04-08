e48d26d56e42ac12cb2e5ba67557cfe77b086268
geodiscovery <ejepujude@gmail.com>
2022-04-07 22:53:03 -0700

    changes to 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
+
+/**
  * main - prints ths phrase in the brackets
  * Returns 0 if successful
  * Else, other output
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
