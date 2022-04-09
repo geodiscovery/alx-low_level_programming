426000991e0689c4b6d907a840f32a98a7f74c2f
geodiscovery <ejepujude@gmail.com>
2022-04-09 09:34:23 -0700

    print alpha 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * program that prints the alphabet in lowercase,
+ * followed by a new line
+ * Return 0 (success)
+ */
+int main(void)
+{
+	char a = 'a';
+	int n = 0;
+
+	while (n < 26)
+
+	{
+		if (a != 'q' && a != 'e')
+		{
+			putchar(a);
+		}
+		a++;
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
